e2a3fb84c36a5fe2ca624eda763544e95ee66b44
Patrick <info@morphp.nl>
2026-05-13 10:34:08 +0200

    Initial commit

---
+++ phpunit.bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Medas\ObjectInstantiator\ObjectInstantiator;
+use Medas\Placeholder\PlaceholderPackage;
+use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+
+chdir(__DIR__);
+
+new ServiceManager(function (): ServiceConfig {
+    $config = new ServiceConfig(ObjectInstantiator::class);
+
+    $config->addPackages([
+        PlaceholderPackage::instance(),
+    ]);
+
+    return $config;
+});
